5bf5710c065c918f4420b701ce0a921b394e7281
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-05-15 23:39:32 -0600

    Libraries

--- src/main.py
+++ src/main.py
@@ -1,5 +1,27 @@
+import sys
+import os
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+
+def get_image_path() -> str:
+    if not len(sys.argv) == 2:
+        raise Exception("Image path not provided")
+
+    image_path = sys.argv[1]
+
+    if not os.path.isfile(image_path):
+        raise Exception("File not found")
+
+    return image_path
+
+
 def main():
-    print("Hi")
+    image_path = get_image_path()
+    image = cv.imread(image_path)
+
+    plt.imshow(image)
+    plt.show()
 
 
 if __name__ == "__main__":
