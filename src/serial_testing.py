743636784e700d8a56ec945073f73712edba1e4b
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-05-23 01:52:11 -0600

    Buffer size testing

--- src/serial_testing.py
+++ src/serial_testing.py
@@ -1,14 +1,16 @@
-from main import Colors, CommandManager
+from serial import Serial
+from time import sleep
 
 
 def main():
-    cm = CommandManager("/dev/ttyUSB0", 9600)
-    cm.move_to(100, 200)
-    cm.move_to(300, 500)
-    cm.change_color(Colors.RED)
-    cm.change_color(Colors.BLUE)
-    cm.tool_up()
-    cm.tool_down()
+    serial = Serial("/dev/ttyUSB0", 9600)
+    sleep(5)
+
+    byts = 0
+    for i in range(100):
+        command = "1" if i % 2 == 0 else "0"
+        byts += serial.write((command + "\n").encode("utf-8"))  # type: ignore
+        print(f"Iteration {i} has {byts} bytes: {round(byts / 64 * 100, 2)}%")
 
 
 if __name__ == "__main__":
